6b60e40b9663d07ce6f2c9425f4240abcb2351f2
siyuanyuchu <2497706711@qq.com>
2016-05-30 19:44:55 +0800

    ATmage256RFR2

---
+++ Mag_Sensor_Driver.h
@@ -0,0 +1,3 @@
+
+void Mag_Sensor_Init (void);
+void Power_On (void);void Power_Off (void);void LED_On (void);void LED_Off (void);void ADC_Init(void);void Mag_X_Init(void);unsigned int Mag_X_Sample(void);void Mag_Y_Init(void);unsigned int Mag_Y_Sample(void);void Mag_Z_Init(void);unsigned int Mag_Z_Sample(void);void USART1_Transmit( unsigned char data );void USART1_Init( unsigned int baudrate );
